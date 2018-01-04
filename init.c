479a213e85ebbf87431b324c1f3f36163ae7496f
Yoan Lecoq <yoanlecoq.io@gmail.com>
2018-01-04 18:04:00 +0100

    Résoudre tout

--- init.c
+++ init.c
@@ -7,11 +7,18 @@
 
 //mettre la valeur de tous ses pointeurs et des cases du plateau à NULL
 void initializerMonde(Monde *monde){
+    // Ou sinon, plus radical et en une seule ligne:
+    // memset(monde, 0, sizeof(*monde));
+    // Cette ligne pourrait remplacer tout le corps de cette fonction.
+
+    for(int y=0 ; y<LONG ; ++y) {
+        for(int x=0 ; x<LARG ; ++x) {
+            monde->plateau[y][x] = NULL;
+        }
+    }
     monde->tour = 0;
-    monde->plateau[LONG][LARG] = NULL;
     monde->rouge = NULL;
     monde->bleu = NULL;
-
 }
 
 //type = GUERRIER ou SERF
@@ -27,35 +34,31 @@ int placerAuMonde(Unite *unite, Monde *monde, int posX, int posY, char couleur){
 	unite->posY = posY;
 	unite->couleur = couleur;
 	
-	monde->plateau[posX][posY] = unite;
+	monde->plateau[posY][posX] = unite;
     return 1; // Elle doit renvoyer quoi cette fonction ?
 }
 
 void ligne(){
 	int i;
-    for(i=0; i<=LONG; i++)
-    {
+    for(i=0; i<LARG; i++) {
         printf("+---");
     }
     printf("+\n");
 }
 
 void afficherPlateau(Monde monde){
-    int i, j;
-	int g=0;
 	printf("  1   2   3   4   5   6   7   8   9  10  11  12\n");
-	for (i = 0; i <= LARG; i++){
-        g++;
+	for (int y = 0; y < LONG; y++){
 		ligne();
-        for (j = 0; j <= LONG; j++){
-            const Unite* u = monde.plateau[i][j];
+        for (int x = 0; x < LARG; x++){
+            const Unite* u = monde.plateau[y][x];
             if(u) {
                 printf("| %c%c ", u->couleur, u->genre);
             } else {
                 printf("| ** "); // u est NULL (case vide)
             }
         }
-        printf("| %d \n", g);
+        printf("| %d \n", y+1);
 	}
 	ligne();
 }
